da2717b5d49a6c9240df34dbfc745dcd2924034f
Daniel Frett <daniel.frett@ccci.org>
2014-08-26 14:07:56 -0400

    [#IDM-322] add support for configuring timeouts for the RestClient

--- src/main/java/me/thekey/cas/client/RestClient.java
+++ src/main/java/me/thekey/cas/client/RestClient.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -18,10 +19,29 @@ import java.net.URLEncoder;
 public class RestClient {
     private static final Logger LOG = LoggerFactory.getLogger(RestClient.class);
 
+    private static final int DEFAULT_CONNECT_TIMEOUT = 2000;
+    private static final int DEFAULT_READ_TIMEOUT = 10000;
+
     private final String casServerUrl;
+
+    private int connectTimeout = DEFAULT_CONNECT_TIMEOUT;
+    private int readTimeout = DEFAULT_READ_TIMEOUT;
 
     public RestClient(final String casServerUrl) {
         this.casServerUrl = casServerUrl + (casServerUrl.endsWith("/") ? "" : "/");
+    }
+
+    public void setConnectTimeout(final int timeout) {
+        this.connectTimeout = timeout;
+    }
+
+    public void setReadTimeout(final int timeout) {
+        this.readTimeout = timeout;
+    }
+
+    public void setTimeout(final int timeout) {
+        this.connectTimeout = timeout;
+        this.readTimeout = timeout;
     }
 
     private String getRestUrl() {
@@ -29,16 +49,15 @@ public class RestClient {
     }
 
     public String getTicketGrantingTicket(final String username, final String password) {
-        URLConnection conn = null;
+        HttpURLConnection conn = null;
         try {
-            conn = new URL(getRestUrl()).openConnection();
-            if (!(conn instanceof HttpURLConnection)) {
-                LOG.error("RestUrl is not an Http url");
+            conn = this.openHttpConnection(new URL(getRestUrl()));
+            if (conn == null) {
                 return null;
             }
 
             // generate POST request
-            ((HttpURLConnection) conn).setRequestMethod("POST");
+            conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             out.write("username=" + URLEncoder.encode(username, "UTF-8") + "&password="
@@ -47,15 +66,15 @@ public class RestClient {
             out.close();
 
             // extract the tgt from the Location header if this was a successful request
-            if (((HttpURLConnection) conn).getResponseCode() == HTTP_CREATED) {
+            if (conn.getResponseCode() == HTTP_CREATED) {
                 final String location = conn.getHeaderField("Location");
                 return location.substring(location.lastIndexOf("/") + 1);
             }
         } catch (final Exception e) {
             LOG.error("error fetching a TicketGrantingTicket", e);
         } finally {
-            if (conn != null && conn instanceof HttpURLConnection) {
-                ((HttpURLConnection) conn).disconnect();
+            if (conn != null) {
+                conn.disconnect();
             }
         }
 
@@ -64,16 +83,15 @@ public class RestClient {
     }
 
     public String getTicket(final String tgt, final String serviceUrl) {
-        URLConnection conn = null;
+        HttpURLConnection conn = null;
         try {
-            conn = new URL(getRestUrl() + "/" + tgt).openConnection();
-            if (!(conn instanceof HttpURLConnection)) {
-                LOG.error("RestUrl is not an Http url");
+            conn = this.openHttpConnection(new URL(getRestUrl() + "/" + tgt));
+            if (conn == null) {
                 return null;
             }
 
             // generate POST request
-            ((HttpURLConnection) conn).setRequestMethod("POST");
+            conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             out.write("service=" + URLEncoder.encode(serviceUrl, "UTF-8"));
@@ -81,7 +99,7 @@ public class RestClient {
             out.close();
 
             // extract the st from the response
-            if (((HttpURLConnection) conn).getResponseCode() == HTTP_OK) {
+            if (conn.getResponseCode() == HTTP_OK) {
                 final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 final String ticket = in.readLine();
                 in.close();
@@ -90,11 +108,27 @@ public class RestClient {
         } catch (final Exception e) {
             LOG.error("error fetching a ServiceTicket", e);
         } finally {
-            if (conn != null && conn instanceof HttpURLConnection) {
-                ((HttpURLConnection) conn).disconnect();
+            if (conn != null) {
+                conn.disconnect();
             }
         }
 
         return null;
+    }
+
+    private HttpURLConnection openHttpConnection(final URL url) throws IOException {
+        // create HttpURLConnection object
+        final URLConnection conn = url.openConnection();
+        if (!(conn instanceof HttpURLConnection)) {
+            LOG.error("'{}' is not an Http url", url);
+            return null;
+        }
+
+        // set connection timeout values
+        conn.setConnectTimeout(this.connectTimeout);
+        conn.setReadTimeout(this.readTimeout);
+
+        // return connection object
+        return (HttpURLConnection) conn;
     }
 }
